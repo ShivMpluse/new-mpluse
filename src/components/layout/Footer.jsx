23e70eefda3e0820cf9b00ca7f7d205c2375d542
ShivMpluse <shivkumar.mpluse@gmail.com>
2025-04-18 10:30:21 +0530

    work at footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import data from '../../footer data/FooterData.js';
+import logo from '../../../public/image/logo-full2.png'
 import { IoLogoInstagram, IoLogoFacebook, IoLogoWhatsapp } from "react-icons/io5";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
@@ -14,54 +14,60 @@ const Footer = () => {
         setEmail("");
     };
 
+
+    let description = `Mpluse Fintech Private Limited is a technology platform that provides API-based financial infrastructure and white-labeled solutions in partnership with licensed banks, NBFCs, and payment aggregators. We are not a bank, NBFC, or payment aggregator and do not hold or claim to hold any license from RBI, NPCI, or any regulatory authority. All financial services and settlements enabled through our platform are operated solely by our regulated partners. Mpluse does not hold end-user funds and acts strictly as a technology facilitator.` 
+
     return (
         <section className='w-full z-50 '>
             <footer className='bg-[#000000] text-white'>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-8 max-w-[1400px] mx-auto">
-                    {data.map((section) => (
-                        <div key={section.id} className='p-2 flex flex-col'>
-                            {section.image && (
-                                <div className="img mb-2 flex flex-col justify-center ">
-                                    <img src={section.image} alt={section.name} className='h-18 w-33 object-contain' />
-                                    <p className="text-justify capitalize text-xs mt-2 text-[#CDCDCD] text-[14px]">{section.description}</p>
-                                    <div className="flex gap-4 text-3xl py-4 justify-evenly">
-                                        <IoLogoInstagram className='text-red-500' />
-                                        <IoLogoFacebook className='text-blue-500' />
-                                        <IoLogoWhatsapp className='text-green-500' />
-                                        <FaTwitter className='text-blue-500' />
-                                        <FaLinkedin className='text-blue-500' />
-                                    </div>
-                                </div>
-                            )}
-                            {section.title && (
-                                <>
-                                    <h3 className="font-bold text-2xl mb-2 px-4 capitalize">{section.title}</h3>
-                                    <ul className="space-y-1 ">
-                                        {section.features?.map((item, idx) => (
-                                            <li key={idx} className="hover:text-gray-400 p-1 text-gray-100 cursor-pointer px-4 mx-auto capitalize">
-                                                <span>{item}</span>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </>
-                            )}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-8 max-w-[1400px] mx-auto">
+                    <div className="img mb-2 flex flex-col justify-center ">
+                        <img src={logo} alt="mpluse" className='h-18 w-33 object-contain' />
+                        <p className="text-justify capitalize text-xs mt-2 text-[#CDCDCD] text-[14px]">{description}</p>
+                        <div className="flex gap-4 text-3xl py-4 justify-evenly">
+                            <IoLogoInstagram className='text-red-500' />
+                            <IoLogoFacebook className='text-blue-500' />
+                            <IoLogoWhatsapp className='text-green-500' />
+                            <FaTwitter className='text-blue-500' />
+                            <FaLinkedin className='text-blue-500' />
                         </div>
-                    ))}
-
+                    </div> 
                     
+                    <div className='border-t-1 md:border-t-0 py-4 md:border-l border-[#777676] pl-4'>
+                        <ul>
+                            <h2 className='text-lg font-semibold mb-2'>Products</h2>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Payment Gateway</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Payouts</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Subscription</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Invoices</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>UPI Solution</li>
+                        </ul>
+                    </div>
+                    <div className='border-t-1 md:border-t-0 py-4 md:border-l border-[#777676] pl-4'>
+                        <ul>
+                            <h2 className='text-lg font-semibold mb-2'>Products</h2>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Payment Gateway</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Payouts</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Subscription</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>Invoices</li>
+                            <li className='text-sm text-[#CDCDCD] mb-1'>UPI Solution</li>
+                        </ul>
+                    </div>
+                    <div className='border-t-1 md:border-t-0 py-4 md:border-l border-[#777676] pl-4'>
+                        <div className="p-2 flex flex-col space-y-3 ">
+                            <h2 className="text-2xl font-semibold mb-2 capitalize">Stay Updated</h2>
+                            <p className='text-sm text-gray-400'>
+                                Subscribe to our newsletter to get latest fintech updates.
+                            </p>
+                            <label className="text-sm">
+                                <input type="checkbox" required /> I agree to the{" "}
+                                <NavLink to="/terms-and-conditions" target='' className="text-blue-500 underline">
+                                    Terms & Conditions
+                                </NavLink>
+                            </label>
+                        </div>
 
-                    {/* 👇 Extra Column for Newsletter */}
-                    <div className="p-2 flex flex-col space-y-3">
-                        <h2 className="text-2xl font-bold mb-2 capitalize">Stay Updated</h2>
-                        <p className='text-sm text-gray-400'>
-                            Subscribe to our newsletter to get latest fintech updates.
-                        </p>
-                        <label className="text-sm">
-                            <input type="checkbox" required /> I agree to the{" "}
-                            <NavLink to="/terms-and-conditions" target='' className="text-blue-500 underline">
-                                Terms & Conditions
-                            </NavLink>
-                        </label>
+
                         <div className="flex flex-col sm:flex-row gap-2">
                             <input
                                 type="email"
@@ -78,6 +84,9 @@ const Footer = () => {
                             </button>
                         </div>
                     </div>
+
+                    {/* 👇 Extra Column for Newsletter */}
+                    
                 </div>
 
                 <div className="flex flex-col items-center py-6 justify-center text-gray-400 border-t border-[#777676] gap-2 text-[14.4px]">
